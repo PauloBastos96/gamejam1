67da8b980d532e62f879fd1d893b6b4968b20ba1
Paulo Bastos <paulosetembro10@hotmail.com>
2024-04-07 00:41:35 +0100

    Player controller done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroKnight : MonoBehaviour {
+
+    [SerializeField] float      m_jumpForce = 7.5f;
+
+    private Animator            m_animator;
+    private Rigidbody2D         m_body2d;
+    private bool                m_grounded = false;
+    private int                 m_currentAttack = 0;
+    private float               m_timeSinceAttack = 0.0f;
+    private int                 m_ColCount = 0;
+    private float               m_DisableTimer;
+
+    private bool State()
+    {
+        if (m_DisableTimer > 0)
+            return false;
+        return m_ColCount > 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        m_ColCount++;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        m_ColCount--;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_ColCount = 0;
+        m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+        m_animator.SetInteger("AnimState", 1);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        // Increase timer that controls attack combo
+        m_timeSinceAttack += Time.deltaTime;
+
+        //Jump timer
+        m_DisableTimer -= Time.deltaTime;
+
+        //Check if character just landed on the ground
+        if (!m_grounded && State())
+        {
+            m_grounded = true;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        //Check if character just started falling
+        if (m_grounded && !State())
+        {
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        //Set AirSpeed in animator
+        m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
+
+        // -- Handle Animations --
+
+        //Attack
+        if(Input.GetButtonDown("Fire1") && m_timeSinceAttack > 0.25f)
+        {
+            m_currentAttack++;
+
+            // Loop back to one after third attack
+            if (m_currentAttack > 3)
+                m_currentAttack = 1;
+
+            // Reset Attack combo if time since last attack is too large
+            if (m_timeSinceAttack > 1.0f)
+                m_currentAttack = 1;
+
+            // Call one of three attack animations "Attack1", "Attack2", "Attack3"
+            m_animator.SetTrigger("Attack" + m_currentAttack);
+
+            // Reset timer
+            m_timeSinceAttack = 0.0f;
+        }
+        else if (Input.GetButtonUp("Fire1"))
+            m_animator.SetBool("IdleBlock", false);
+            
+        //Jump
+        else if (Input.GetButtonDown("Jump") && m_grounded)
+        {
+            m_animator.SetTrigger("Jump");
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+            m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
+            DisableJump(0.2f);
+        }
+    }
+
+    /// <summary>
+    /// Disable jump for a certain duration
+    /// </summary>
+    /// <param name="duration">The duration for which the player can't jump</param>
+    public void DisableJump(float duration)
+    {
+        m_DisableTimer = duration;
+    }
+}
