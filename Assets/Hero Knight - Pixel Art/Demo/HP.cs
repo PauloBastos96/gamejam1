2bf4f24a4bfeb7a4db947a3c65476bc7cf4b4468
Pedrocasss <pedroacnesisma13@gmail.com>
2024-04-06 21:10:57 +0100

    hp animation done

--- Assets/Hero Knight - Pixel Art/Demo/HP.cs
+++ Assets/Hero Knight - Pixel Art/Demo/HP.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     
     // Start is called before the first frame update
-    public int HP = 3;
+    public Sprite[] array_sprite;
+    public Image canvas_image;
+    public int HP = 4;
     void Start()
     {
         
@@ -15,6 +18,23 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F)) {
+            HP--;
+            switch (HP)
+            {
+                case 3:
+                    canvas_image.sprite = array_sprite[array_sprite.Length - 1];
+                    break;
+                case 2:
+                    canvas_image.sprite = array_sprite[array_sprite.Length - 2];
+                    break;
+                case 1:
+                    canvas_image.sprite = array_sprite[array_sprite.Length - 3];
+                    break;
+                default:
+                    canvas_image.sprite = array_sprite[array_sprite.Length - 4];
+                    break;
+            }
+        }
     }
 }
